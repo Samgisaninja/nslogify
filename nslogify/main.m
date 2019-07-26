eaa37738c72e40c69c0ece0629e37112c6b4c5a5
Sam Gardner <stgardner4@att.net>
2019-07-26 14:46:33 -0500

    Initial Commit

---
+++ nslogify/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nslogify
+//
+//  Created by Sam Gardner on 7/26/19.
+//  Copyright © 2019 Sam Gardner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
